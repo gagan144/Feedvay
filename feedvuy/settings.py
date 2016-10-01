a2f415d1a6f7bca1e55c06dc323748aeb452e79e
pycharm_gagan <noone@mail.com>
2016-10-01 20:36:13 +0530

    Project foundation: django admin docs

--- feedvuy/settings.py
+++ feedvuy/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django.contrib.admindocs',
 ]
 
 MIDDLEWARE = [

a1b542b9df043269d58a885d04b5de3a6f5172fd
pycharm_gagan <noone@mail.com>
2016-10-19 00:01:37 +0530

    Project Foundation: Utilities - Api utils

---
+++ utilities/api_utils.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2016 Feedvuy (Gagandeep Singh: singh.gagan144@gmail.com) - All Rights Reserved
+# Content in this document can not be copied and/or distributed without the express
+# permission of Gagandeep Singh.
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse
+import ujson
+
+class ApiResponse(object):
+    """
+    A class to create a json response. Mostly used to cater API requests.
+
+    **Authors**: Gagandeep Singh
+    """
+
+    # --- enums ---
+    ST_SUCCESS = 'success'
+    ST_IGNORED = 'ignored'        # No update or change
+    ST_NOT_ALLOWED = 'not_allowed'    # GET or POST not allowed
+    ST_FORBIDDEN = 'forbidden'        # Access is not allowed or prohibited
+    ST_UNAUTHORIZED = 'unauthorized ' # Authentication fails
+    ST_BAD_REQUEST = 'bad_request'    # Bad request formation/Invalid or missing parameteres
+    ST_SERVER_ERROR = 'server_error'  # Any internal server error
+
+    CH_STATUS = (
+        (ST_SUCCESS, 'Success'),
+        (ST_IGNORED, 'Ignored'),
+        (ST_NOT_ALLOWED, 'Not Allowed'),
+        (ST_FORBIDDEN, 'Forbidden'),
+        (ST_UNAUTHORIZED, 'Unauthorized'),
+        (ST_BAD_REQUEST, 'Bad request'),
+        (ST_SERVER_ERROR, 'Server error'),
+    )
+
+    STATUS_CODES = {
+        ST_SUCCESS: 200,
+        ST_IGNORED: 204,
+        ST_NOT_ALLOWED: 405,
+        ST_FORBIDDEN: 403,
+        ST_UNAUTHORIZED: 401,
+        ST_BAD_REQUEST: 400,
+        ST_SERVER_ERROR: 500
+    }
+
+    # --- Fields ---
+    __data = {}
+
+    @property
+    def code(self):
+        return self.__data['code']
+
+    @property
+    def status(self):
+        return self.__data['status']
+
+    @property
+    def data(self):
+        return self.__data
+
+    def __init__(self, status, message, **kwargs):
+        data = {
+
+            "status": status,
+            "message": message
+        }
+
+        data.update(**kwargs)
+
+        # Validate
+        self.__validate(data)
+
+        data["code"] = ApiResponse.STATUS_CODES[status]
+        self.__data = data
+
+    def __validate(self, data):
+        status = data['status']
+        is_valid = False
+        for k,v in ApiResponse.CH_STATUS:
+            if k == status:
+                is_valid = True
+                break
+
+        if not is_valid:
+            raise ValidationError("Invalid status '{}'.".format(status))
+
+    # --- Methods ---
+    def set(self, key, value):
+        """
+        Method to add or update more key-value to response json.
+        """
+        if key in ['code', 'status']:
+            raise Exception("Key should be one of 'code', 'status'")
+
+        self.__data[key] = value
+
+    def change_status(self, status):
+        """
+        Method to change status.
+        """
+        data = self.__data.copy()
+        data['status'] = status
+
+        # Validate
+        self.__validate(data)
+
+        self.__data['status'] = status
+        self.__data['code'] = ApiResponse.STATUS_CODES[status]
+
+
+    def gen_http_response(self):
+        """
+        Method to generate http response instance. Return this from the view.
+
+        :return: Returns :class:`django.http.JsonResponse` instance
+        """
+
+        return JsonResponse(self.__data)

1400003f222d20c30ff9433b6dd28da1d60e5bba
pycharm_gagan <noone@mail.com>
2016-11-14 22:41:02 +0530

    Console: Brand switching middleware

--- console/middleware.py
+++ console/middleware.py
@@ -2,9 +2,12 @@
 # Content in this document can not be copied and/or distributed without the express
 # permission of Gagandeep Singh.
 from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpResponseForbidden, HttpResponseRedirect
 from django.http.response import Http404
+from django.core.urlresolvers import reverse
 
-from brands.models import Brand
+from accounts.models import RegisteredUser
+from brands.models import Brand, BrandOwner
 
 
 class ConsoleBrandSwitchMiddleware(MiddlewareMixin):
@@ -31,26 +34,54 @@ class ConsoleBrandSwitchMiddleware(MiddlewareMixin):
     """
     def process_request(self, request):
         curr_url = request.path
+
+        # Only consider '/console/b/.....' url, bypass otherwise
         if curr_url.startswith('/console/b/'):
-            split_url = curr_url.split('/')
+            # Check if user is logged in
+            if request.user.is_authenticated():
+                split_url = curr_url.split('/')
 
-            if split_url[2] == 'b':
-                # Get current brand
-                brand_uid = split_url[3]
-                try:
-                    request.curr_brand = Brand.objects.get(brand_uid=brand_uid)
+                if split_url[2] == 'b':
+                    # Get brand_uid from url
+                    brand_uid = split_url[3]
+                    try:
+                        # Check if user is registered user
+                        reg_user = request.user.registereduser
 
-                    # Create new path by stripping off /b/<brand_uin>/ from curr_url
-                    split_url.__delitem__(3)
-                    split_url.__delitem__(2)
+                        # Fetch brand from db
+                        brand = Brand.objects.get(brand_uid=brand_uid)
 
-                    new_path = '/'.join(split_url)
+                        try:
+                            # Check ownership
+                            BrandOwner.objects.get(brand_id=brand.id, registered_user_id=reg_user.id)
 
-                    # request.path = new_path   # DANGER: Using this creates endless looping
-                    request.path_info = new_path
-                except ValueError:
-                    # ValueError: badly formed hexadecimal UUID string
-                    raise Http404("Invalid brand link.")
-                except Brand.DoesNotExist:
-                    raise Http404("Invalid brand link.")
+                            # Set brand in request object so that it can be used in templates
+                            request.curr_brand = brand
+                        except BrandOwner.DoesNotExist:
+                            # Ownership not found; Return forbidden response
+                            return HttpResponseForbidden('You do not have permissions to access this page.')
 
+                        # Everything ok here now
+                        # Create new path by stripping off '/b/<brand_uid>/' from curr_url
+                        split_url.__delitem__(3)
+                        split_url.__delitem__(2)
+
+                        new_path = '/'.join(split_url)
+
+                        # DANGER: using 'request.path = new_path' creates endless looping
+                        # Override request path so that view can be resolved
+                        request.path_info = new_path
+                    except RegisteredUser.DoesNotExist:
+                        # User is not a registered user
+                        return HttpResponseForbidden('You do not have permissions to access this page.')
+                    except ValueError:
+                        # Invalid uuid for brand_uid, throws 'ValueError: badly formed hexadecimal UUID string'
+                        raise Http404("Invalid brand link.")
+                    except Brand.DoesNotExist:
+                        # Brand was not found
+                        raise Http404("Invalid brand link.")
+            else:
+                # /if request.user.is_authenticated():
+                # Not authenticated; redirect to login
+                # Since login url does not begins with '/console/b/', this middleware will bypass check
+                return HttpResponseRedirect(reverse('accounts_login') + '?next=' + curr_url)

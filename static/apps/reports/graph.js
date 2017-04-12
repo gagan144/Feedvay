0d2df1abf16dca069ef5830d857c1f6ad85a825b
Gagandeep <singh.gagan144@gmail.com>
2017-04-12 22:09:24 +0530

    Reports: Graphs - Stats

--- static/apps/reports/graph.js
+++ static/apps/reports/graph.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('feedvay.reports.graphs', [] )
+angular.module('feedvay.reports.graphs', [])
 
 // ---------- Services ----------
 .service('ServiceGraph', function($http){
@@ -16,7 +16,7 @@ angular.module('feedvay.reports.graphs', [] )
         return $http.get('/reports/api/graph_data/' + graph_uid + '/', {
             params: params
         }).then(function (response) {
-            return response.data.objects;
+            return response.data;
         });
     }
 })
@@ -36,7 +36,9 @@ angular.module('feedvay.reports.graphs', [] )
         scope: {
             config: "=config"
         },
-        controller: function ($scope, ServiceGraph) {
+        controller: function ($scope, $rootScope, ServiceGraph) {
+            $scope.ST_AJAX = ST_AJAX;
+
             // $scope.config already defined
             var org_uid = $scope.config['c'];
             var graph_uid = $scope.config['graph_uid'];
@@ -47,6 +49,7 @@ angular.module('feedvay.reports.graphs', [] )
                 status: null,
                 error_msg: null
             };
+            $scope.flags.status = ST_AJAX.LOADING;
 
             $scope.data = null;
             $scope.get_data = function(){
@@ -54,7 +57,8 @@ angular.module('feedvay.reports.graphs', [] )
 
                 ServiceGraph.get_data(org_uid, graph_uid, $scope.filters).then(
                     function(response_data){
-                        $scope.flags.status = null;
+                        $scope.flags.status = ST_AJAX.COMPLETED;
+                        $scope.data = response_data.data;
                     },
                     function(response_data){
 
@@ -62,23 +66,24 @@ angular.module('feedvay.reports.graphs', [] )
 
                 );
             };
+            $scope.get_data();
 
 
 
 
 
 
-            console.info("enter directive controller");
-            console.log($scope.config);
-
-            $scope.set = function(){
-                $scope.data.name = 'deep';
-            };
-
-            $scope.data = {
-                "name": "gagan",
-                "config": $scope.config
-            };
+            //console.info("enter directive controller");
+            //console.log($scope.config);
+            //
+            //$scope.set = function(){
+            //    $scope.data.name = 'deep';
+            //};
+            //
+            //$scope.data = {
+            //    "name": "gagan",
+            //    "config": $scope.config
+            //};
 
             //$http({method: 'GET', url: $scope.src}).then(function (result) {
             //    console.log(result);
